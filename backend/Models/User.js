aae1456a5b5e9da9fd16b6164101f3386e61a7fe
devhimanshuu <devhimanshuu@gmail.com>
2025-11-08 04:02:42 +0530

    Entities

---
+++ backend/Models/User.js
@@ -0,0 +1,50 @@
+// models/user.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+    password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false,
+  },
+  avatar: {
+    type: String,
+    default: false,
+  },
+   isPro:{
+    type: Boolean,
+    default: false,
+   },
+},
+  { timestamps: true });
+
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//method to compare password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
